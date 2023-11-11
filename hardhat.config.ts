0def60f46959b4337327c5584e6bff13180738ef
Sahil sen <sensahil25@gmail.com>
2023-11-11 19:28:47 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,33 @@
+import * as dotenvenc from '@chainlink/env-enc'
+dotenvenc.config();
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+//import './tasks'
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const POLYGON_MUMBAI_RPC_URL = process.env.POLYGON_MUMBAI_RPC_URL;
+const AVALANCHE_FUJI_RPC_URL = process.env.AVALANCHE_FUJI_RPC_URL;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+
+    polygonMumbai: {
+      url: POLYGON_MUMBAI_RPC_URL !== undefined ? POLYGON_MUMBAI_RPC_URL : "",
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 80001,
+    },
+    avalancheFuji: {
+      url: AVALANCHE_FUJI_RPC_URL !== undefined ? AVALANCHE_FUJI_RPC_URL : "",
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 43113,
+      allowUnlimitedContractSize: true,
+    },
+  },
 };
 
 export default config;
